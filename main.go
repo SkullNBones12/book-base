bdc6df6ad54a5b63fdaba11bb9a7e41855bbdaed
Joseph C <jo.ciesielczyk@gmail.com>
2024-03-05 08:21:43 -0500

    Json downloaded, builds made

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Docs struct {
+	Arrays []any `json:"docs"`
+}
+
+type Name struct {
+	Names []any `json:"author_name"`
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	response, err := http.Get("https://openlibrary.org/search.json?title=the+lord+of+the+rings")
+
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	defer response.Body.Close()
+
+	responseData, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(string(responseData))
 }
